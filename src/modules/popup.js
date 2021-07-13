9f8247a7d9e5b7b83c4e5e1c56b4c2b7a7200090
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-07-13 21:08:46 +0300

    Реализация попапа, плавного скролла, авто-слайдера

---
+++ src/modules/popup.js
@@ -0,0 +1,31 @@
+const togglePopup = () => {
+  const buttonCallback = document.querySelectorAll('.button-callback');
+  const popupNode = document.querySelector('.modal-callback');
+  const popupOverlay = document.querySelector('.modal-overlay');
+
+  buttonCallback.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      evt.preventDefault();
+
+      console.log('l');
+      if (evt.target) {
+        popupNode.style.display = 'block';
+        popupOverlay.style.display = 'block';
+      }
+    });
+  });
+
+  document.addEventListener('click', (evt) => {
+    const target = evt.target;
+
+    if (
+      target.classList.contains('modal-close') ||
+      target.classList.contains('modal-overlay')
+    ) {
+      popupNode.style.display = 'none';
+      popupOverlay.style.display = 'none';
+    }
+  });
+};
+
+export { togglePopup };
